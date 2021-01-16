bacb513eae4a4249caf5627bd83ae3a9f4d980d1
Andrei Tumbar <andreitumbar@gmail.com>
2021-01-16 15:41:17 -0500

    Don't give out a reference to the parser

--- test/integration_test.c
+++ test/integration_test.c
@@ -8,62 +8,62 @@
 #define CTEST(name) static void name(void** state)
 
 // Pretend header
-void* calc_init();
+uint32_t calc_init();
 void* calc_allocate_buffers();
 void calc_free_buffers(void* self);
-void calc_free(void* self);
-double calc_parse(const void* self, const void* buffers, const char* input);
+void calc_free();
+double calc_parse(const void* buffers, const char* input);
 
-void* calc_ascii_init();
+uint32_t calc_ascii_init();
 void* calc_ascii_allocate_buffers();
 void calc_ascii_free_buffers(void* self);
-void calc_ascii_free(void* self);
-double calc_ascii_parse(const void* self, const void* buffers, const char* input);
+void calc_ascii_free();
+double calc_ascii_parse(const void* buffers, const char* input);
 
 CTEST(test_empty)
 {
-    void* parser = calc_init();
+    assert_int_equal(calc_init(), 0);
     void* buffers = calc_allocate_buffers();
 
-    assert_double_equal(calc_parse(parser, buffers, "   "), 0, 0);
+    assert_double_equal(calc_parse(buffers, "   "), 0, 0);
 
-    calc_free(parser);
+    calc_free();
     calc_free_buffers(buffers);
 }
 
 CTEST(test_parser)
 {
-    void* parser = calc_init();
+    assert_int_equal(calc_init(), 0);
     void* buffers = calc_allocate_buffers();
 
     const char* input = "3 + 5 + (4 * 2 + (5 / 2))";
-    double result = calc_parse(parser, buffers, input);
+    double result = calc_parse(buffers, input);
     assert_double_equal(result, 3 + 5 + (4 * 2 + (5.0 / 2)), 0.001);
     calc_free_buffers(buffers);
-    calc_free(parser);
+    calc_free();
 }
 
 CTEST(test_empty_ascii)
 {
-    void* parser = calc_ascii_init();
+    assert_int_equal(calc_ascii_init(), 0);
     void* buffers = calc_ascii_allocate_buffers();
 
-    assert_double_equal(calc_ascii_parse(parser, buffers, "   "), 0, 0);
+    assert_double_equal(calc_ascii_parse(buffers, "   "), 0, 0);
 
-    calc_ascii_free(parser);
+    calc_ascii_free();
     calc_ascii_free_buffers(buffers);
 }
 
 CTEST(test_parser_ascii)
 {
-    void* parser = calc_ascii_init();
+    assert_int_equal(calc_ascii_init(), 0);
     void* buffers = calc_ascii_allocate_buffers();
 
     const char* input = "3 + 5 + (4 * 2 + (5 / 2))";
-    double result = calc_ascii_parse(parser, buffers, input);
+    double result = calc_ascii_parse(buffers, input);
     assert_double_equal(result, 3 + 5 + (4 * 2 + (5.0 / 2)), 0.001);
     calc_ascii_free_buffers(buffers);
-    calc_ascii_free(parser);
+    calc_ascii_free();
 }
 
 const static struct CMUnitTest left_scan_tests[] = {
