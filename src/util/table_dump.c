18dc31402099f26d18e7164678a1e8f63cbb3d94
Andrei Tumbar <andreitumbar@gmail.com>
2021-02-26 13:57:48 -0500

    Add GPL-3 licenses

--- src/util/table_dump.c
+++ src/util/table_dump.c
@@ -1,3 +1,20 @@
+/* 
+ * This file is part of the Neoast framework
+ * Copyright (c) 2021 Andrei Tumbar.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <parsergen/canonical_collection.h>
 
 void dump_item(const LR_1* lr1, uint32_t tok_n, const char* tok_names, FILE* fp)
